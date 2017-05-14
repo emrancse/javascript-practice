f41e7f683a9b817d7b22637577fc39d66fc82d7a
emrancse <emranict@gmail.com>
2017-05-15 01:53:00 +0600

    Get a random number by using Math.random()

---
+++ js random number.js
@@ -0,0 +1,17 @@
+// get a random number
+
+var getRandomNumber = Math.random();
+
+console.log(getRandomNumber);
+
+
+// Now get random number between 0 to 9
+
+var secondRandomNumber = Math.floor(getRandomNumber * 10);
+
+console.log(secondRandomNumber);
+
+// Now get random number from 1 to 100
+
+var thirdRandomNumber = Math.floor(getRandomNumber * 100);
+console.log(thirdRandomNumber);
