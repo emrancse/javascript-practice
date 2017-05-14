e22d314e63ea43e73d4d52de2f6d2962f19c22b1
emrancse <emranict@gmail.com>
2017-05-14 16:45:56 +0600

    Conversion decimal number to binary, octal and hexdecimal

---
+++ javascriptnumberconversion.js
@@ -0,0 +1,9 @@
+var decimalNumber = 12;
+var binary = decimalNumber.toString(2);
+console.log("The equivalent binary of " + decimalNumber + " is " + binary);
+
+var octal = decimalNumber.toString(8);
+console.log("The equivalent octal of " + decimalNumber + " is " + octal);
+
+var hexadecimal = decimalNumber.toString(16);
+console.log("The equivalent hexadecimal of " + decimalNumber + " is " + hexadecimal);
