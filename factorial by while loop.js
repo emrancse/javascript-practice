0683f3bc6bac913da30f7d4af1cb45226db65064
emrancse <emranict@gmail.com>
2017-05-23 16:24:52 +0600

    Find a factorial by while loop

---
+++ factorial by while loop.js
@@ -0,0 +1,16 @@
+// Find a factorial by while loop
+
+function factorialize(num){
+  var result = num;
+  if(num === 0 || num ===1){
+    return 1;
+  }
+  
+  while(num>1){
+    num--;
+    result *= num;
+  }
+  return result;
+}
+
+console.log(factorialize(5));
