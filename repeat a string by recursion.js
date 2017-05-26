b34c79378c55f6e12b1ab5a45abea687af3cfe7b
emrancse <emranict@gmail.com>
2017-05-26 14:09:10 +0600

    repeat a string by recursion

---
+++ repeat a string by recursion.js
@@ -0,0 +1,11 @@
+// repeat a string by recursion
+
+function repeatStringNumTimes(str, num) {
+  if(num < 0) 
+    return "";
+  if(num === 1) 
+    return str;
+  else 
+    return str + repeatStringNumTimes(str, num - 1);
+}
+console.log(repeatStringNumTimes("abc", 3));
