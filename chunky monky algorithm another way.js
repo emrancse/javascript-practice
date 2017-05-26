ee92eafefd2aaa950567f05f979ab215bcb9fafe
emrancse <emranict@gmail.com>
2017-05-26 23:22:37 +0600

    Chunky monky algorithm by another way

---
+++ chunky monky algorithm another way.js
@@ -0,0 +1,14 @@
+// Chunky Monkey algorithom by another way
+
+function chunkArrayInGroups(arr, size) {
+  // Break it up.
+  var group = [];
+  var position = 0;
+  
+  while(position < arr.length) {
+    group.push(arr.slice(position, position+=size));
+  }
+  return group;
+}
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
