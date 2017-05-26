b86b8cfdf0fa4e7a852d31ac834a604ef658c9ac
emrancse <emranict@gmail.com>
2017-05-26 23:33:52 +0600

    Slasher flick aglorithm in freecodecamp

---
+++ slasher flick.js
@@ -0,0 +1,10 @@
+// Slasher Flick
+
+function slasher(arr, howMany) {
+  // remove the head
+  arr.splice(0, howMany);
+  // return the remaining or the tail
+  return arr;
+}
+
+console.log(slasher([1, 2, 3], 2));
