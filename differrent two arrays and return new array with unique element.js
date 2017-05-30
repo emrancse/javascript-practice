c146191e881c3440869d740ee48d9d1a23a78001
emrancse <emranict@gmail.com>
2017-05-30 16:48:23 +0600

    differrent two arrays and return new arrary with unique element

---
+++ differrent two arrays and return new array with unique element.js
@@ -0,0 +1,22 @@
+// Differnece two arrays and return new array with new unique element
+
+function diffArray(arr1, arr2) {
+  var newArr = [];
+ 
+  for(var i = 0; i < arr1.length; i++){
+    
+    if(arr2.indexOf(arr1[i]) === -1){
+      newArr.push(arr1[i]);
+    }
+  }
+  
+  for(var j = 0; j < arr2.length; j++){
+    if(arr1.indexOf(arr2[j]) === -1){
+      newArr.push(arr2[j]);
+    }
+  }
+  
+  return newArr;
+}
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
